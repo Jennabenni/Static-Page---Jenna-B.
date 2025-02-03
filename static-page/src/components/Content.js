467fa8bc7687d2f2fb6bc5daa848274e2dbcd284
Jennabenni <je686804@ucf.edu>
2025-02-03 18:02:13 -0500

    commit

---
+++ static-page/src/components/Content.js
@@ -0,0 +1,13 @@
+//2-3 paragraph summary
+import React from 'react';
+
+//needs to say content since it's
+//referenced in App.js
+function Content(){
+    return(
+        <body>
+            <p>Text</p>
+        </body>
+    );
+}
+export default Content;
---
+++ static-page/src/components/Header.js
@@ -0,0 +1,13 @@
+//Title of story
+//I need basically a heading
+
+import React from 'react';
+
+function Header(){
+    return(
+        <header>
+            <h1>Cinderella</h1>
+        </header>
+    );
+}
+export default Header;
